8d58298de9fb14d755355e01ce1274f327c56c88
Roopasree7 <roopasree1607@gmail.com>
2025-07-22 00:03:49 +0530

    updated

--- frontend/frontend/src/components/PointBadge.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const PointsBadge = ({ streak, points }) => (
-  <div className="points-badge">
-    🔥 Streak: {streak} days | ⭐ Points: {points}
-  </div>
-);
-
-export default PointsBadge;
